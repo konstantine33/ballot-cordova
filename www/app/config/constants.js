2059245270b5436845acaf0fbaab4f2830c83c4d
Lingke <wlingke@gmail.com>
2015-01-07 21:11:43 -0800

    minor fixes

--- www/app/config/constants.js
+++ www/app/config/constants.js
@@ -1,4 +1,4 @@
 BallotApp
-    .constant('SERVER_URL', 'http://ballot-server.herokuapp.com')
-    //.constant('SERVER_URL', 'http://localhost:3000')
+    .constant('SERVER_URL', 'http://tallyhere.com/api')
+    //.constant('SERVER_URL', 'http://localhost:3000/api')
     .constant('$ionicLoadingConfig', {template: '<i class="icon ion-loading-c"></i> Loading...'});
